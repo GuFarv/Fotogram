0989d110e15a0ccbc1d4e0e4c816c43869b73b97
Guenther Farveleder <guenther.farveleder@gmail.com>
2026-01-18 19:01:36 +0100

    functionality of upload and hover effect

--- imgview.js
+++ imgview.js
@@ -1,48 +1,109 @@
 const images = [
-"./img/gallery/1_img.jpeg",
-"./img/gallery/2_img.jpeg",
-"./img/gallery/3_img.jpeg",
-"./img/gallery/4_img.jpeg",
-"./img/gallery/5_img.jpeg",
-"./img/gallery/6_img.jpeg",
-"./img/gallery/7_img.jpeg",
-"./img/gallery/8_img.jpeg",
-"./img/gallery/9_img.jpeg",
-"./img/gallery/10_img.jpeg",
-"./img/gallery/11_img.jpeg",
-"./img/gallery/12_img.jpeg",
+    //Preview Funktion
+
+    "./img/gallery/1_img.jpeg",
+    "./img/gallery/2_img.jpeg",
+    "./img/gallery/3_img.jpeg",
+    "./img/gallery/4_img.jpeg",
+    "./img/gallery/5_img.jpeg",
+    "./img/gallery/6_img.jpeg",
+    "./img/gallery/7_img.jpeg",
+    "./img/gallery/8_img.jpeg",
+    "./img/gallery/9_img.jpeg",
+    "./img/gallery/10_img.jpeg",
+    "./img/gallery/11_img.jpeg",
+    "./img/gallery/12_img.jpeg",
 ];
+
 
 let currentIndex = 0;
 
 function openLightbox(index) {
-currentIndex = index;
-document.getElementById("lightbox").style.display = "flex";
-updateImage();
+    currentIndex = index;
+    document.getElementById("lightbox").style.display = "flex";
+    updateImage();
 }
 
 function closeLightbox() {
-document.getElementById("lightbox").style.display = "none";
+    document.getElementById("lightbox").style.display = "none";
 }
 
 function changeImage(direction) {
-currentIndex += direction;
+    currentIndex += direction;
 
-if (currentIndex < 0) currentIndex = images.length - 1;
-if (currentIndex >= images.length) currentIndex = 0;
+    if (currentIndex < 0) currentIndex = images.length - 1;
+    if (currentIndex >= images.length) currentIndex = 0;
 
-updateImage();
+    updateImage();
 }
 
 function updateImage() {
-document.getElementById("lightbox-img").src = images[currentIndex];
+    document.getElementById("lightbox-img").src = images[currentIndex];
 }
 
 
 document.addEventListener("keydown", (e) => {
-if (document.getElementById("lightbox").style.display === "flex") {
-if (e.key === "ArrowRight") changeImage(1);
-if (e.key === "ArrowLeft") changeImage(-1);
-if (e.key === "Escape") closeLightbox();
-}
+    if (document.getElementById("lightbox").style.display === "flex") {
+        if (e.key === "ArrowRight") changeImage(1);
+        if (e.key === "ArrowLeft") changeImage(-1);
+        if (e.key === "Escape") closeLightbox();
+    }
 });
+
+//Upload Funktion
+
+const input = document.getElementById("files");
+const thumbs = document.getElementById("thumbs");
+
+input.addEventListener("change", () => {
+
+
+    [...input.files].forEach(file => {
+        const img = document.createElement("img");
+        img.src = URL.createObjectURL(file);
+        img.style.width = "150px";
+        img.style.height = "150px";
+        img.style.objectFit = "cover";
+        img.style.paddingLeft = "10px";
+        img.style.paddingRight = "10px";
+        img.style.borderRadius = "25px";
+        thumbs.appendChild(img);
+    });
+});
+
+
+function drawThumbnail(ctx, img, size) {
+    const ratio = Math.max(size / img.width, size / img.height);
+    const w = img.width * ratio;
+    const h = img.height * ratio;
+    const x = (size - w) / 2;
+    const y = (size - h) / 2;
+
+    ctx.drawImage(img, x, y, w, h);
+}
+
+//Bilderspeicher löschen
+
+function img_delete() {
+    thumbs.innerHTML = "";
+    upload.value = "";
+    return
+}
+
+
+//Bilderanzahl pro Zeile
+
+
+const maxPerRow = 7;
+
+function renderThumbnail(file, index) {
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(file);
+
+
+    if ((index + 1) % maxPerRow === 0) {
+        img.style.marginRight = "0";
+    }
+
+    gallery.appendChild(img);
+}
